e6d3488ee221121aad43a8de94e089c89c33655e
David Barton <david@i4-software.co.uk>
2015-08-26 17:22:07 +0100

    Added theme switcher

--- src/main/java/uk/co/i4software/poppie/main/MainController.java
+++ src/main/java/uk/co/i4software/poppie/main/MainController.java
@@ -22,6 +22,12 @@ public class MainController implements Serializable {
 
     private static final long serialVersionUID = 56485545678274039L;
 
+    private static final String[] THEMES = { "afterdark", "afternoon", "afterwork", "aristo", "black-tie", "blitzer",
+            "bluesky", "bootstrap", "casablanca", "cupertino", "cruze", "dark-hive", "delta", "dot-luv", "eggplant",
+            "excite-bike", "flick", "glass-x", "home", "hot-sneaks", "humanity", "le-frog", "midnight", "mint-choc",
+            "overcast", "pepper-grinder", "redmond", "rocket", "sam", "smoothness", "south-street", "start", "sunny",
+            "swanky-purse", "trontastic", "ui-darkness", "ui-lightness", "vader" };
+
     @Inject
     private Census census;
 
@@ -31,5 +37,9 @@ public class MainController implements Serializable {
 
     public FactType[] getFactTypes() {
         return census.fetchFactTypes();
+    }
+
+    public String[] getThemes() {
+        return THEMES;
     }
 }
